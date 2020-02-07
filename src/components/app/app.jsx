ee783f872c9dca66ba7901129ce90255fc8f6727
boris <borisdiaw12@gmail.com>
2020-02-07 12:52:34 +0200

    Create components. Add first prop.

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from "../main/main.jsx";
+// eslint-disable-next-line react/prop-types
+const App = ({offersCount}) => {
+  return <Main
+    offersCount={offersCount}
+  />;
+};
+
+export default App;
---
+++ src/components/main/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// eslint-disable-next-line react/prop-types
+const Main = ({offersCount}) => {
+  return (<div>Number of offers {offersCount}</div>);
+};
+
+export default Main;
